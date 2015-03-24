bf0b64955d138f28479323f1f891c0780efd0394
Raphael Rodrigues Coelho <rapha.rodriguescoelho@gmail.com>
2015-03-24 14:21:52 -0300

    Solving identation

--- scripts/character.js
+++ scripts/character.js
@@ -2,11 +2,11 @@ Character.prototype = new Element();
 Character.prototype.constructor = Character;
 
 function Character() {
-	this._TOTAL_LIFE		= 100;
-	this._DEFEATED_LIFE		= 6;
+	this._TOTAL_LIFE	= 100;
+	this._DEFEATED_LIFE	= 6;
 	this._DESTROYED_LIFE	= 0;
 	this._action            = Actions.flying;
-	this.count				= 0;
+	this.count		= 0;
 	
 	this._explode = function() {
 		this._action.isExploding() ? this._changeImage(this.image.transition)
